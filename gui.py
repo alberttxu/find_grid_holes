5fae025a90fa6d6f2cc42a982d73a29b6f49b14d
Albert Xu <albert.t.xu@gmail.com>
2018-06-11 14:17:41 -0400

    added basic crop

--- gui.py
+++ gui.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QAction, 
+from PyQt5.QtCore import Qt, QRect, QSize
+from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QAction,
                              QHBoxLayout, QVBoxLayout, QGridLayout,
-                             QLabel, QScrollArea, QPushButton, QFileDialog, QCheckBox, QSlider, QLCDNumber, QLineEdit)
+                             QLabel, QScrollArea, QPushButton, QFileDialog,
+                             QCheckBox, QSlider, QLineEdit, QRubberBand)
 from PyQt5.QtGui import QPixmap, QKeySequence
 
 
@@ -27,13 +28,13 @@ class ImageViewer(QScrollArea):
         self.pixmap.load(filename)
         self.originalCopy = self.pixmap
         self.label.setPixmap(self.pixmap)
-        #self.label.resize(self.label.sizeHint())
         self.label.resize(self.pixmap.size())
 
     def _refresh(self):
-        
-        self.pixmap = self.originalCopy.scaled(self.zoomScale * self.originalCopy.size(),
-                                         aspectRatioMode = Qt.KeepAspectRatio)
+        self.pixmap = self.originalCopy.scaled(
+                                         self.zoomScale
+                                         * self.originalCopy.size(),
+                                         aspectRatioMode=Qt.KeepAspectRatio)
         self.label.setPixmap(self.pixmap)
         self.label.resize(self.pixmap.size())
 
@@ -44,6 +45,31 @@ class ImageViewer(QScrollArea):
     def zoomOut(self):
         self.zoomScale -= 0.1
         self._refresh()
+
+
+class ImageViewerCrop(ImageViewer):
+
+    def __init__(self, filename=''):
+        super().__init__(filename)
+
+    def mousePressEvent (self, eventQMouseEvent):
+        self.originQPoint = eventQMouseEvent.pos()
+        self.currentQRubberBand = QRubberBand(QRubberBand.Rectangle, self)
+        self.currentQRubberBand.setGeometry(QRect(self.originQPoint, QSize()))
+        self.currentQRubberBand.show()
+
+    def mouseMoveEvent (self, eventQMouseEvent):
+        self.currentQRubberBand.setGeometry(
+                QRect(self.originQPoint, eventQMouseEvent.pos()).normalized())
+
+    def mouseReleaseEvent (self, eventQMouseEvent):
+        self.currentQRubberBand.hide()
+        currentQRect = self.currentQRubberBand.geometry()
+        self.currentQRubberBand.deleteLater()
+        cropQPixmap = self.label.pixmap().copy(currentQRect)
+        cropQPixmap.save('output.png')
+
+        self.parentWidget().sidebar.crop_template.loadPicture('output.png')
 
 
 class Sidebar(QWidget):
@@ -57,38 +83,39 @@ class Sidebar(QWidget):
         self.setFixedWidth(self.width)
 
         # widgets
-        crop_template = ImageViewer('output.png')
-        crop_template.setFixedHeight(200)
+        self.crop_template = ImageViewer('output.png')
+        self.crop_template.setFixedHeight(200)
         blur_template = QCheckBox('Blur template')
         blur_img  = QCheckBox('Blur image')
         buttonAutoDoc = QPushButton('Generate autodoc file')
         buttonAutoDoc.resize(buttonAutoDoc.sizeHint())
         buttonPrintCoord = QPushButton('Print Coordinates')
         buttonPrintCoord.resize(buttonPrintCoord.sizeHint())
-
         slider = QSlider(Qt.Horizontal)
         self.thresholdDisplay = QLineEdit()
         slider.valueChanged.connect(self.changeThreshDisp)
-        threshold = QGridLayout()
-        threshold.addWidget(slider, 0, 0, 1, 1)
-        threshold.addWidget(self.thresholdDisplay, 1, 1, 1, 1)
-        
+
         # layout
         vlay = QVBoxLayout()
-        vlay.addWidget(crop_template)
+        vlay.addWidget(self.crop_template)
         vlay.addWidget(blur_template)
         vlay.addWidget(blur_img)
+        vlay.addWidget(QLabel())
+
         vlay.addWidget(QLabel('Threshold'))
+        threshold = QGridLayout()
+        threshold.addWidget(slider, 0, 0, 1, 1)
+        threshold.addWidget(self.thresholdDisplay, 1, 0, 1, 1)
+        threshold.addWidget(QLabel())
         vlay.addLayout(threshold)
-        #vlay.addWidget(slider)
-        #vlay.addWidget(self.thresholdDisplay)
+
         vlay.addWidget(buttonAutoDoc)
         vlay.addWidget(buttonPrintCoord)
         vlay.addStretch(1)
         self.setLayout(vlay)
 
     def changeThreshDisp(self, i: int):
-        self.thresholdDisplay.setText(str(i))
+        self.thresholdDisplay.setText("%.2f" % (i/100))
 
 
 class MainWidget(QWidget):
@@ -99,24 +126,15 @@ class MainWidget(QWidget):
 
     def initUI(self):
         self.sidebar = Sidebar()
-        self.viewer = ImageViewer()
-        
+        self.viewer = ImageViewerCrop()
+
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
         self.grid.addWidget(self.sidebar, 1, 0, 1, 1)
         self.grid.addWidget(self.viewer, 1, 1, 5, 5)
         self.setLayout(self.grid)
 
-    def loadPicture(self, filename):
-        self.viewer.loadPicture(filename)
-        
-    def viewZoomIn(self):
-        self.viewer.zoomIn()
-        
-    def viewZoomOut(self):
-        self.viewer.zoomOut()
-        
-        
+
 class Window(QMainWindow):
 
     def __init__(self):
@@ -139,10 +157,10 @@ class Window(QMainWindow):
 
         zoomIn = QAction("Zoom In", self)
         zoomIn.setShortcut(Qt.Key_Equal)
-        zoomIn.triggered.connect(self.viewZoomIn)
+        zoomIn.triggered.connect(self.root.viewer.zoomIn)
         zoomOut = QAction("Zoom Out", self)
         zoomOut.setShortcut(Qt.Key_Minus)
-        zoomOut.triggered.connect(self.viewZoomOut)
+        zoomOut.triggered.connect(self.root.viewer.zoomOut)
         viewMenu.addAction(zoomIn)
         viewMenu.addAction(zoomOut)
 
@@ -151,22 +169,17 @@ class Window(QMainWindow):
         self.show()
 
     def openFileDialog(self):
-        filename = QFileDialog.getOpenFileName(self, 'Open File', '/home/albertxu/git_projects/find_grid_holes')[0]
+        filename = QFileDialog.getOpenFileName(self, 'Open File',
+                            '/home/albertxu/git_projects/find_grid_holes')[0]
         #filename = QFileDialog.getOpenFileName(self, 'Open File', '/')[0]
 
         print(filename)
         if filename:
-            self.root.loadPicture(filename)
+            self.root.viewer.loadPicture(filename)
 
-    def viewZoomIn(self):
-        self.root.viewZoomIn()
-        
-    def viewZoomOut(self):
-        self.root.viewZoomOut()
-        
 
 if __name__ == '__main__':
-        
+
     app = QApplication(sys.argv)
     w = Window()
     sys.exit(app.exec_())
